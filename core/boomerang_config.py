7dd85214e9e20c5aab9e820f5c6049b1af469540
alexangrlt <alexangirault@gmail.com>
2026-01-19 19:32:02 +0100

    fix pb indent spaces/tabs

--- core/boomerang_config.py
+++ core/boomerang_config.py
@@ -11,19 +11,19 @@ class BoomerangConfig:
     """Settings Boomerang 2 pales (rectangulaires)"""
 
     # Parametres Geometriques
-    L = 0.195  # longueur de pale en m																		#? mesuré
-    l = 0.034  # largeur de pale en m																		#? mesuré
-    h = 0.0031  # epaisseur de pale en m																	#? mesuré
+    L = 0.195  # longueur de pale en m                                                                      #? mesuré
+    l = 0.034  # largeur de pale en m                                                                       #? mesuré
+    h = 0.0031  # epaisseur de pale en m                                                                    #? mesuré
 
-    R_pale = 0.112  # rayon (centre boomerang jusqu'aux extremités des pales) en m						    #? mesuré
-    rho_boomerang = 1.24  # densité du filament en g/cm³ donc en kg/dm³ donc kg/L							#? donnée vendeur (cura)
+    R_pale = 0.112  # rayon (centre boomerang jusqu'aux extremités des pales) en m                          #? mesuré
+    rho_boomerang = 1.24  # densité du filament en g/cm³ donc en kg/dm³ donc kg/L                           #? donnée vendeur (cura)
     N_troncons = 20  # ? arbitraire
     angles_pales = (
         0,
         74,
     )  # angle de la 1ere pale et de la deuxieme en degré                                                   #? par calcul avec trigos et dessin du boomerang
-    c_root = 0.042  # largeur de la racine (à 0.0015 m du bout) en m #chord									#? mesuré
-    c_tip = 0.033  # largeur du bout des pales (à 0.001 m du bout) en m #chord								#? mesuré
+    c_root = 0.042  # largeur de la racine (à 0.0015 m du bout) en m #chord                                 #? mesuré
+    c_tip = 0.033  # largeur du bout des pales (à 0.001 m du bout) en m #chord                              #? mesuré
 
     e = np.linspace(
         0.005,  # on enleve l'epaisseur de debut, d'ou le 0.005 (sinon on va avoir une epaisseur infinie)
@@ -32,7 +32,7 @@ class BoomerangConfig:
     )
 
     # Masse
-    masse = 0.028  # masse en kg #4eme boomerang (1000%, 100% infill (triangles), 3h40 d'impression, PLA)	#? donnée par le slicer u(m)=0.002        #faudrait que je le pèse réellement mais pour le code je pense pas que ça change grand chose...
+    masse = 0.028  # masse en kg #4eme boomerang (1000%, 100% infill (triangles), 3h40 d'impression, PLA)   #? donnée par le slicer u(m)=0.002        #faudrait que je le pèse réellement mais pour le code je pense pas que ça change grand chose...
 
     # Parametres Aerodynamiques
     rho_air = 1.225  # densité de l'air en kg/m³
